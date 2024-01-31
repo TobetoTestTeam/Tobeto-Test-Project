fee5f497d62149042f586902501aa732448526f8
KubraIsik <kuisik@gmail.com>
2024-02-01 00:12:07 +0300

    add .gitignore and .env file

--- constants/globalConstants.py
+++ constants/globalConstants.py
@@ -1,6 +1,8 @@
 # login
-USER_MAIL = "pair4testteam@gmail.com"
-USER_PASSWORD = "P123456."
+import os
+
+USER_MAIL = os.environ.get("USER_MAIL") 
+USER_PASSWORD = os.environ.get("USER_PASSWORD")
 
 # test_addExperience.py constants
 USER_PROFILE_BUTTON_XPATH = "//*[@id='__next']/div/nav/div[1]/div/div/div[2]"

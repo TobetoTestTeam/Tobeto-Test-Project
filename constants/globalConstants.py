017a1be79dc33eb5d0faeda6fa07a2cbb2d7e96a
KubraIsik <kuisik@gmail.com>
2024-01-31 21:28:13 +0300

    test account info changed

--- constants/globalConstants.py
+++ constants/globalConstants.py
@@ -1,6 +1,6 @@
 # login
-USER_MAIL = "icimdekisi@gmail.com"
-USER_PASSWORD = "1234567_"
+USER_MAIL = "pair4testteam@gmail.com"
+USER_PASSWORD = "P123456."
 
 # test_addExperience.py constants
 USER_PROFILE_BUTTON_XPATH = "//*[@id='__next']/div/nav/div[1]/div/div/div[2]"
